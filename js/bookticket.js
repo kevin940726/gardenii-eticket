00ce80d38d503be8877d5f0846be790ab69ef242
kevin940726 <kevin830726@gmail.com>
2015-05-28 23:36:14 +0800

    Fix bugs, add material design

--- js/bookticket.js
+++ js/bookticket.js
@@ -1,6 +1,6 @@
 
 $( document ).ready(function() {
-	var seatlists=['A1','A2','B1','B2'];
+	var seatlists=[];
 	var activitylists=['活動1','活動2','活動3','活動4','活動5']
 	for(var i = 0; i<4; i++){
 		$('#seatareaA').append("<div class='col-md-1 seat'><a href='javascript:;'>"+"A"+i+"</a></div>");
@@ -8,34 +8,32 @@ $( document ).ready(function() {
 		$('#seatareaC').append("<div class='col-md-1 seat'><a href='javascript:;'>"+"C"+i+"</a></div>");
 		$('#seatareaD').append("<div class='col-md-1 seat'><a href='javascript:;'>"+"D"+i+"</a></div>");		
 	}
-	for(var i = 0; i<4; i++){
-		$('#disemail').append("<label for='inputEmail' class='col-sm-2 control-label'>"+seatlists[i]+"</label><div class='col-sm-10'><input type='email' class='form-control' id='inputEmail' placeholder='請輸入Email'></div>");
-	}	
-	for(var i = 0; i<5; i++){
-		$('#activitycontainer').append("<button type='button' class='btn btn-default btn-lg btn-block'>"+activitylists[i]+"</button>");
-	}	
-	for(var i = 0; i<seatlists_distribute.length; i++){
-		$('#myseats').append("<div class='col-md-1 seat_distribute'><a href='javascript:;'>"+seatlists_distribute[i]+"</a></div>");
-	}    
+	// for(var i = 0; i<4; i++){
+	// 	$('#disemail').append("<label for='inputEmail' class='col-sm-2 control-label'>"+seatlists[i]+"</label><div class='col-sm-10'><input type='email' class='form-control' id='inputEmail' placeholder='請輸入Email'></div>");
+	// }	
+	// for(var i = 0; i<5; i++){
+	// 	$('#activitycontainer').append("<button type='button' class='btn btn-default btn-lg btn-block'>"+activitylists[i]+"</button>");
+	// }	
+	// for(var i = 0; i<seatlists_distribute.length; i++){
+	// 	$('#myseats').append("<div class='col-md-1 seat_distribute'><a href='javascript:;'>"+seatlists_distribute[i]+"</a></div>");
+	// }    
 	$('.seat').on( "click", function(){
-
 		if(seatlists.indexOf($(this).text())>-1){
 			$(this).css('background','white');
 	    	seatlists.splice(seatlists.indexOf($(this).text()),1);
-	    	console.log('seatlists',seatlists);		
-	    	
-	  } 
-    else{
-    	$(this).css('background','pink');
-    	seatlists.push($(this).find('a').text());
-    	console.log('seatlists',seatlists);
-    }
+	    	console.log('seatlists',seatlists);				    	
+		} 
+	    else{
+	    	$(this).css('background','pink');
+	    	seatlists.push($(this).find('a').text());
+	    	console.log('seatlists',seatlists);
+	    }
 	});
 
 	$('#ticketform #savechange').on( "click", function(){
     	$('#ticketform #inputPosition').val(seatlists);
 	});
-	$('#ticketform').click(function(){
+	$('#ticketform').submit(function(){
 		var email = $("#ticketform input#inputEmail").val();
 		var name = $("#ticketform input#inputName").val();
 		var phone = $("#ticketform input#inputPhone").val();
