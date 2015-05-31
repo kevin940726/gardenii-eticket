6a6a047f83ca0cb6af8e1f0410ab616ebd44d5a0
ivorybangle <ivorybangle99@gmail.com>
2015-05-31 18:17:12 +0800

    change css js path

--- views/partials/ticketrecord.php
+++ views/partials/ticketrecord.php
@@ -12,19 +12,18 @@
     <title>E-ticket</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="../css/bootstrap.min.css" rel="stylesheet">
-    <link href="../css/material-fullpalette.min.css" rel="stylesheet">
-    <link href="../css/material.min.css" rel="stylesheet">
-    <link href="../css/ripples.min.css" rel="stylesheet">
-    <link href="../css/roboto.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/material-fullpalette.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/material.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/ripples.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/roboto.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
-    <link href="../css/app.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/app.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
-
+    <script src="/gardenii-eticket/js/ie-emulation-modes-warning.js"></script>
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
@@ -80,10 +79,12 @@
 
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-    <script src="../js/ie10-viewport-bug-workaround.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script src="../js/material.min.js"></script>
-    <script src="../js/ripples.min.js"></script>
+    <script src="/gardenii-eticket/assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="/gardenii-eticket/assets/js/bootstrap.min.js"></script>
+    <script src="/gardenii-eticket/assets/js/material.min.js"></script>
+    <script src="/gardenii-eticket/assets/js/ripples.min.js"></script>
+    <script src="/gardenii-eticket/assets/js/bookticket.js"></script>
+    <script src="/gardenii-eticket/assets/js/fblogin.js"></script>
     <script>
       $.material.init();
     </script>
