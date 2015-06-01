6987b3b17afe59442c5ae664828246a4cd7d43da
ivorybangle <ivorybangle99@gmail.com>
2015-06-01 15:39:19 +0800

    commit

---
+++ views/main/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="E-ticket prototype">
+    <meta name="author" content="NTU IM 105">
+    <link rel="icon" href="../../favicon.ico">
+
+    <title>E-ticket</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="/gardenii-eticket/assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/material-fullpalette.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/material.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/ripples.min.css" rel="stylesheet">
+    <link href="/gardenii-eticket/assets/css/roboto.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="/gardenii-eticket/assets/css/app.css" rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+    <script src="/gardenii-eticket/js/ie-emulation-modes-warning.js"></script>
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    <div id="fb-root"></div>
+    <div class="container">
+      <div class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li id='memberinfoli' role="presentation" sytle='display:none'><a id='memberinfo' href="../member/manage_event">會員資訊</a></li>
+            <li role="presentation"><a id='login' href="../main/index"></a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted"><a href='../main/index'>Gardenii</a></h3>
+      </div>
+  </head>
