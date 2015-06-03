91012e92b1504fe7d8bd0282556e84e99caf8048
Geng-Yu Laing <liveman24224@gmail.com>
2015-06-04 00:50:29 +0800

    fb

---
+++ application/config/facebook.php
@@ -0,0 +1,9 @@
+<?php
+$config['facebook']['api_id'] = '1458009167830285';
+$config['facebook']['app_secret'] = '91151d813757b270b6400d9e80d3a772';
+$config['facebook']['redirect_url'] = current_url();
+$config['facebook']['permissions'] = array(
+  'email',
+  'user_location',
+  'user_birthday'
+);
