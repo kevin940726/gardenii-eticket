86d9ffdab9b01cd5c0fddbd96e0e547782fa0d4e
weichun <weichun@getmore.tw>
2015-06-04 01:31:39 +0800

    register

--- views/main/register.php
+++ views/main/register.php
@@ -28,6 +28,18 @@
               <input type="text" class="form-control" id="inputEmailReg"></input>
             </div>
           </div>
+          <br>
+          <div class="form-group">
+            <ul>
+              <li>會員資料請詳實填寫，如有錯誤、不實而導致損失，會員應自行負責。</li>
+              <li>嚴禁以不同身分重複註冊，註冊資料經查證後如非屬實，本公司有權取消其會員資格。若您冒用他人名義造成損失，網站將保留法律追訴權。</li>
+              <li>註冊會員後，請務必妥善保管帳號及密碼，且不得供他人使用，以維護您本身權益。若會員未能妥善保存帳號資料而致權益受損，本公司概不負責。</li>
+              <li>您在網站所登錄或留存之個人資料將嚴密受到保護，未經您同意，本公司將不會挪做他途。</li>
+            </ul>
+            <div>
+              <input type="checkbox">同意</input>
+            </div>
+          </div>
           <div class="form-group">
             <div style='text-align:center'>
               <button type='submit' class="btn btn-primary" data-toggle="modal" data-target="#register-dialog">註冊</button>
@@ -48,13 +60,9 @@
               </div>
             </div>
           </div>
-
         </form>
 
-
-
       <?php $this->load->view("/footer.php"); ?>
-
     
     <script src="/gardenii-eticket/assets/js/register.js"></script>
 
--- views/member/contract.php
+++ views/member/contract.php
@@ -1,30 +1,19 @@
 
 <?php $this->load->view("/header.php"); ?>
   <body>
-
-   
       <div>
-      <ul>
-      <li>會員資料請詳實填寫，如有錯誤、不實而導致損失，會員應自行負責。</li>
-      <li>  
-嚴禁以不同身分重複註冊，註冊資料經查證後如非屬實，本公司有權取消其會員資格。若您冒用他人名義造成損失，網站將保留法律追訴權。
-  </li>
-  <li>  
-註冊會員後，請務必妥善保管帳號及密碼，且不得供他人使用，以維護您本身權益。若會員未能妥善保存帳號資料而致權益受損，本公司概不負責。
-
-  </li>
-  <li>  
-您在網站所登錄或留存之個人資料將嚴密受到保護，未經您同意，本公司將不會挪做他途。
-  </li>
-      </ul>
+        <ul>
+          <li>會員資料請詳實填寫，如有錯誤、不實而導致損失，會員應自行負責。</li>
+          <li>嚴禁以不同身分重複註冊，註冊資料經查證後如非屬實，本公司有權取消其會員資格。若您冒用他人名義造成損失，網站將保留法律追訴權。</li>
+          <li>註冊會員後，請務必妥善保管帳號及密碼，且不得供他人使用，以維護您本身權益。若會員未能妥善保存帳號資料而致權益受損，本公司概不負責。</li>
+          <li>您在網站所登錄或留存之個人資料將嚴密受到保護，未經您同意，本公司將不會挪做他途。</li>
+        </ul>
       </div>
       <div style='text-align:center'>
         <button id='contractagree' class="btn btn-primary">同意</button>
       </div>
+      <?php $this->load->view("/footer.php"); ?>
 
-     <?php $this->load->view("/footer.php"); ?>
-
-    <script src="/gardenii-eticket/assets/js/contract.js"></script>
-
+      <script src="/gardenii-eticket/assets/js/contract.js"></script>
   </body>
 </html>
