60e1b26cdbce61ae458d668ad867cf38b3e82eac
Geng-Yu Laing <liveman24224@gmail.com>
2015-06-10 15:01:09 +0800

    api

--- application/models/API_Model.php
+++ application/models/API_Model.php
@@ -19,8 +19,22 @@ class API_Model extends CI_Model {
 
             $query = $this->db->get();
             foreach( $query->result_array() as $row ){
+                $temp = array();
+                foreach ($row as $key => $value){
+                    if(
+                        $key=='event_title' || 
+                        $key=='event_description' ||
+                        $key=='event_location' ||
+                        $key=='event_host'     
+                    ){
+                        $temp[$key] = urlencode($value);
+                    }
+                    else{
 
-                array_push($result, $row);
+                        $temp[$key] = $value;
+                    }
+                }
+                array_push($result, $temp);
             }
         }
         return $result;
